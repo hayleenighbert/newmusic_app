f40cececc3463d18f24eb6026497b9b7feda417b
Haylee Nighbert <haylee.nigh@gmail.com>
2015-11-03 17:10:07 -0800

    added profile page

--- controllers/login.js
+++ controllers/login.js
@@ -6,11 +6,20 @@ router.get("/", function(req, res) {
 	res.render("index");
 });
 
-router.get("/login", function(req, res) {
+// router.get("/login", function(req, res) {
+// 	if (req.currentUser) {
+// 		res.render("/login");
+// 	} else {
+// 		res.redirect("/profile");
+// 	}
+// });
+
+router.get("/profile", function(req, res) {
 	if (req.currentUser) {
-		res.render("/login");
+		console.log()
+		res.render("profile");
 	} else {
-		res.redirect("/");
+		res.redirect("/")
 	}
 });
 
