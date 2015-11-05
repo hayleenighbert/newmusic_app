7e1f7fa09850e33bf786ebafe383966133698411
Haylee Nighbert <haylee.nigh@gmail.com>
2015-11-05 13:22:19 -0800

    changes

--- controllers/music.js
+++ controllers/music.js
@@ -12,17 +12,18 @@ router.get('/results', function(req, res){
         var searchTerm = req.query.s;
         request("http://developer.echonest.com/api/v4/artist/search?"+process.env.API_KEY+"&format=json&name=" + searchTerm, 
         function (error, response, data){
+            console.log(JSON.parse(data));
             res.render('musicresults', {myData: JSON.parse(data)})
         })
     });
 
-router.get('/mood', function(req, res){
-        var searchTerm = req.query.s;
-        request("http://developer.echonest.com/api/v4/artist/search?api_key="+process.env.API_KEY+"&format=json&mood=" + searchTerm, 
-        function (error, response, data){
-            res.render('musicresults', {myData: JSON.parse(data)})
-        })
-    });
+// router.get('/mood', function(req, res){
+//         var searchTerm = req.query.s;
+//         request("http://developer.echonest.com/api/v4/artist/search?api_key="+process.env.API_KEY+"&format=json&mood=" + searchTerm, 
+//         function (error, response, data){
+//             res.render('musicresults', {myData: JSON.parse(data)})
+//         })
+//     });
 
 
 // router.get('/results', function(req, res){
