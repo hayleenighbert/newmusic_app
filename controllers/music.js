431c1032e4103b65cee943c44bac26ee43952862
Haylee Nighbert <haylee.nigh@gmail.com>
2015-11-04 17:44:36 -0800

    updated

--- controllers/music.js
+++ controllers/music.js
@@ -6,17 +6,11 @@ var
 
 router.get('/', function(req, res){
     res.render('music')
-//     http.get("http://developer.echonest.com/api/v4/artist/search?api_key=ZJZ4R41VXOVHQNL6P&format=json&name=radiohead&results=1", 
-//         function (response){
-//             response.on('data', function (chunk){
-//                 res.send(JSON.parse(chunk));
-//             })
-//         });
 });
 
 router.get('/results', function(req, res){
         var searchTerm = req.query.s;
-        request("http://developer.echonest.com/api/v4/artist/search?api_key=ZJZ4R41VXOVHQNL6P&format=json&name=" + searchTerm, 
+        request("http://developer.echonest.com/api/v4/artist/search?"+process.env.API_KEY+"&format=json&name=" + searchTerm, 
         function (error, response, data){
             res.render('musicresults', {myData: JSON.parse(data)})
         })
