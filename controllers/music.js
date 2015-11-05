0c5641ada0e82291a0ab0ccb86087d4f3890d052
Haylee Nighbert <haylee.nigh@gmail.com>
2015-11-05 14:14:03 -0800

    mood

--- controllers/music.js
+++ controllers/music.js
@@ -10,20 +10,20 @@ router.get('/', function(req, res){
 
 router.get('/results', function(req, res){
         var searchTerm = req.query.s;
-        request("http://developer.echonest.com/api/v4/artist/search?"+process.env.API_KEY+"&format=json&name=" + searchTerm, 
+        request("http://developer.echonest.com/api/v4/artist/search?api_key="+process.env.API_KEY+"&format=json&name=" + searchTerm, 
         function (error, response, data){
             console.log(JSON.parse(data));
             res.render('musicresults', {myData: JSON.parse(data)})
         })
     });
 
-// router.get('/mood', function(req, res){
-//         var searchTerm = req.query.s;
-//         request("http://developer.echonest.com/api/v4/artist/search?api_key="+process.env.API_KEY+"&format=json&mood=" + searchTerm, 
-//         function (error, response, data){
-//             res.render('musicresults', {myData: JSON.parse(data)})
-//         })
-//     });
+router.get('/mood/:mood', function(req, res){
+        var searchTerm = req.params.mood;
+        request("http://developer.echonest.com/api/v4/artist/search?api_key="+process.env.API_KEY+"&format=json&mood=" + searchTerm, 
+        function (error, response, data){
+            res.render('musicresults', {myData: JSON.parse(data)})
+        })
+    });
 
 
 // router.get('/results', function(req, res){
